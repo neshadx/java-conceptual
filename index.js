66084cc2fcdba72995e8165ddda8603a30eac6b3
neshadx <neshadimagify@gmail.com>
2025-02-16 22:05:53 +0600

    added some code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*
+**1. Variable
+
 var name = "Neshad";
 
 
@@ -27,3 +30,26 @@ var convert = parseFloat(num1);
 var result = convert + num2;
 
 console.log(result);
+ */
+
+/**
+ * 2. Conditions
+ var balance = 10000;
+if (balance > 5000 && balance < 10000) {
+    console.log("I will buy Shirt");
+}
+else if (balance >= 10000) {
+    console.log("I will buy pant")
+} 
+else {
+    console.log("I don't go market");
+}
+
+ */
+
+
+var num1 = 10;
+if (num1 >= 5 || num1 == 0) {
+    console.log("Yes sir");
+}
+
