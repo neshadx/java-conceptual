90170316a070ac3075cea40cbfc4153e3dd9f3fc
neshadx <neshadimagify@gmail.com>
2025-02-16 21:22:06 +0600

    added index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+var name = "Neshad";
+
+
+name += " Mahmud";
+console.log(name);
+
+var age = 27;
+age = age + 2;
+console.log(age);
+
+var isAge = true;
+console.log(typeof isAge);
+
+
+var num1 = "500";
+var num2 = 200;
+
+var convert = parseInt(num1);
+var result = convert + num2;
+
+console.log(result);
+
+var num1 = "500.678";
+var num2 = 200;
+
+var convert = parseFloat(num1);
+var result = convert + num2;
+
+console.log(result);
