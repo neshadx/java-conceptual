aa8bc09fdfeab1aa7298495c832ee389c5a03043
neshadx <neshadimagify@gmail.com>
2025-02-17 20:54:44 +0600

    added some code

---
+++ object.js
@@ -0,0 +1,13 @@
+/**
+
+ */
+const nesh = {
+    name: "Neshad",
+    age: 27,
+    relation: "single",
+}
+
+// const name = nesh.age;
+// const key = Object.keys(nesh);
+// const value = Object.values(nesh);
+console.log(name);
